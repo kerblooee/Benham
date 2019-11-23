13d916bc2550121c522b08e8a4bacee25893bdc6
Glitch (caterwauling-loganberry) <none>
2019-11-23 07:23:44 +0000

    🌨😂 Updated with Glitch

--- script.js
+++ script.js
@@ -28,24 +28,37 @@ function preload() {
 function create() {
   this.graphics = this.add.graphics({ lineStyle: {color: 0xFFFFFF, width: 5}, fillStyle: {color: 0xFFFFFF, alpha: 1} });
   this.arcs = this.add.graphics({ lineStyle: {color: 0x000000, width: 3}, fillStyle: {color: 0x000000, alpha: 1} });  
+  this.control = this.add.graphics({ lineStyle: {color: 0x00ff55, width: 1}});
   // create control bar
-  this.controller = new Phaser.Geom.Line(40,100,80,100);
+  this.controller = new Phaser.Geom.Line(50,100,70,100);
+  this.divisionsBar = new Phaser.Geom.Line(90,100,110,100);
+  this.lengthBar = new Phaser.Geom.Line(75,10,75,30);
+  // turn on inputs
   this.input.on('pointerup', onPointerUp, this); 
   this.input.on('pointerdown', onPointerDown, this);
-  
+  this.speedDial = new Phaser.Geom.Line(60,50,60,250);
+  this.divisionsDial = new Phaser.Geom.Line(100,50,100,250);
+  this.lengthDial= new Phaser.Geom.Line(50,20,250,20);
 };
 
 //commented out until I want to work more on inputs
 
 function onPointerMove(pointer) {
-  //this.line.setTo(this.x, this.y, pointer.x, pointer.y);
-  
-  this.controller.setTo(40,pointer.y,80,pointer.y);
+  if((pointer.x<250&&pointer.x>50)&&(pointer.y<45&&pointer.y>5)){    
+    this.lengthBar.setTo(pointer.x,10,pointer.x,30);
+    this.lBar = (pointer.x-50)/2;
+  };  
+
+  if((pointer.x<80&&pointer.x>40)&&(pointer.y<250&&pointer.y>50)){    
+    this.controller.setTo(50,pointer.y,70,pointer.y);
+    this.y = pointer.y;
+  };
+  if((pointer.x<120&&pointer.x>80)&&(pointer.y<250&&pointer.y>50)){
+    this.divisionsBar.setTo(90,pointer.y,110,pointer.y);
+    this.div=((pointer.y-50)/25)+2;
+  }
   console.log('moving', pointer.x, ' ', pointer.y);
-  this.y = pointer.y;
-  this.graphics.strokeLineShape
-  // Write portion that will limit where it can be dragged.
-  // Numbers that represent the speed would be helpful.
+  
 }
 
 function onPointerDown(pointer)  {
@@ -170,6 +183,8 @@ function spin() {
 };
 
 function spin2 () {
+  //  Try to set  this.y  with an initial value
+ if(this.y==null){this.y=100,this.div = 5,this.lBar = 0};
 const time = +((new Date)*(this.y/200))%360;
   // Clears the graphics
   this.graphics.clear();
@@ -180,29 +195,56 @@ const time = +((new Date)*(this.y/200))%360;
   // Use these variables for adjustable portions
   var r1, r2, r3;
   var ang1, ang2, ang3;
-  
   // Draw black arcs  
-    for(var i=0;i<4;i++) {
+  //   for(var i=0;i<4;i++) {
+  // this.arcs.beginPath();
+  // this.arcs.arc(
+  //   config.width/2,
+  //   config.height/2,
+  //   config.height/2-22.5-35*i,
+  //   Phaser.Math.DegToRad(0+time+(90/4)*i),
+  //   Phaser.Math.DegToRad((90/4)+time+(90/4)*i),
+  //   false
+  // );
+  // this.arcs.strokePath();
+  // //console.log("I made ", i, " black arcs");
+  //   };
+  //   for(var i=0;i<4;i++) {
+  // this.arcs.beginPath();
+  // this.arcs.arc(
+  //   config.width/2,
+  //   config.height/2,
+  //   config.height/2-22.5-35*i,
+  //   Phaser.Math.DegToRad(180+time+(90/4)*i),
+  //   Phaser.Math.DegToRad(180+(90/4)+time+(90/4)*i),
+  //   false
+  // );
+  // this.arcs.strokePath();
+  // //console.log("I made ", i, " black arcs on the other side");
+  //   };
+  
+  // Draw black arcs with custom divisions prototyping
+    for(var i=0;i<this.div;i++) {
   this.arcs.beginPath();
   this.arcs.arc(
     config.width/2,
     config.height/2,
-    config.height/2-22.5-35*i,
-    Phaser.Math.DegToRad(0+time+(90/4)*i),
-    Phaser.Math.DegToRad((90/4)+time+(90/4)*i),
+    (i*(config.height-10)/2+80)/this.div,
+    Phaser.Math.DegToRad(time+(90/this.div)*i),
+    Phaser.Math.DegToRad(time+(90/this.div)*(1+i)+this.lBar),
     false
   );
   this.arcs.strokePath();
-  //console.log("I made ", i, " black arcs");
+  //console.log("I made ", i, " black arcs ", this.div);
     };
-    for(var i=0;i<4;i++) {
+    for(var i=0;i<this.div;i++) {
   this.arcs.beginPath();
-  this.arcs.arc(
+  this.arcs.arc(    
     config.width/2,
     config.height/2,
-    config.height/2-22.5-35*i,
-    Phaser.Math.DegToRad(180+time+(90/4)*i),
-    Phaser.Math.DegToRad(180+(90/4)+time+(90/4)*i),
+    (i*(config.height-10)/2+80)/this.div,
+    Phaser.Math.DegToRad(180+time+(90/this.div)*i),
+    Phaser.Math.DegToRad(180+time+(90/this.div)*(1+i)+this.lBar),
     false
   );
   this.arcs.strokePath();
@@ -240,11 +282,18 @@ function art() {
 
 function controller() {
   this.graphics.strokeLineShape(this.controller);
+  this.graphics.strokeLineShape(this.divisionsBar);
+  this.graphics.strokeLineShape(this.lengthBar);
+  this.control.strokeLineShape(this.speedDial);
+  this.control.strokeLineShape(this.divisionsDial);
+  this.control.strokeLineShape(this.lengthDial);
+
 };
 
 function update() {
+
   //spin.call(this);
   spin2.call(this);
   //art.call(this);
-  controller.call(this);
+  controller.call(this);  
 };
